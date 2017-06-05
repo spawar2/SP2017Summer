06992cf40e6be1096d197c60c4a2c3432a03ba68
GSU <spawar2@student.gsu.edu>
2017-06-05 09:57:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gsu/sp2017summer/NewActivity.java
@@ -0,0 +1,17 @@
+package com.example.gsu.sp2017summer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GSU on 6/5/17.
+ */
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
